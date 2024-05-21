74e757ada4e717aa080522e4fc4b45ea18720469
VR <evaosa2011@gmail.com>
2024-05-21 17:18:11 +0500

    nbhbgvfvc

---
+++ Assets/Eva/Scripts/Carry.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carry : MonoBehaviour
+{
+   
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"));
+        Destroy(col.gameObject);
+    }
+
+    void 
+       OnTriggerEnter(Collider col)
+    {
+        if (gameObject.CompareTag("Finish"));
+        Destroy(col.gameObject);
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"));
+        Destroy(col.gameObject);
+    }
+    }
